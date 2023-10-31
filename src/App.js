e4b329f057ac298c7fe1e2691f2acad2245c8b31
project733 <project733@proton.me>
2023-10-30 17:58:34 -0700

    KL - 003 - initial design update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/global.scss";
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.container}>
+      <header className={styles.fade}>
+        <h1 className={styles.myname}>Kevin Leung</h1>
+        <h2 className={styles.myskills}>Web/HTML Programmer</h2>
+        <p className={styles.mywork}>See what I worked on</p>
+      </header>
+      <main className={styles.projects}>
+        <section
+          className={styles.fade}
+          style={{ background: "lightgray" }}
+        ></section>
+        <section
+          className={styles.fade}
+          style={{ background: "lightpink" }}
+        ></section>
+        <section
+          className={styles.fade}
+          style={{ background: "darkgoldenrod" }}
+        ></section>
+        <section
+          className={styles.fade}
+          style={{ background: "lightskyblue" }}
+        ></section>
+        <section
+          className={styles.fade}
+          style={{ background: "lightcoral" }}
+        ></section>
+        <section
+          className={styles.fade}
+          style={{ background: "gold" }}
+        ></section>
+      </main>
+      {/* <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +47,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
